8118c0894b1eb8c0fc9d3255ac2c77b52762c3c5
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2022-12-28 23:44:37 +0500

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {
 "assets/FontManifest.json": "312f32cbee9acddd2ee5fc0ac0713c58",
 "assets/fonts/Caramel-Regular.ttf": "224ef2230e4d11b90bfd395039ea9768",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "115ad866c426e6cb83ea895ca524bb46",
+"assets/NOTICES": "e80843ae88de875cbddbf7b2a437d7f6",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -60,9 +60,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f97be12d50c3827ecd61ff16ed67bcbb",
-"/": "f97be12d50c3827ecd61ff16ed67bcbb",
-"main.dart.js": "963e7afecdf0835d0c0198a7b0725f22",
+"index.html": "0cc7406cb1f64832e2e64025ff527112",
+"/": "0cc7406cb1f64832e2e64025ff527112",
+"main.dart.js": "4f075a6255d6ee2569f488a94e9fed2f",
 "manifest.json": "748eef23665d06d71d890000a4ad0ab7",
 "version.json": "b07421f69d245996e47fbe6d359625d1"
 };
